b8a1c0bd97d1154b60464112ad39a612ea7094ad
maramrmilad7 <maro12787@gmail.com>
2025-08-15 21:23:14 +0200

    adding data to categoreis model to use it later

--- lib/Models/category_model.dart
+++ lib/Models/category_model.dart
@@ -3,10 +3,13 @@ class CategoryModel {
   final String? image;
 
   CategoryModel({required this.title, required this.image});
-  factory CategoryModel.fromJson( json) {
-    return CategoryModel(
-      title: json['title'] ?? '',
-      image: json['image'] ?? '',
-    );
-  }
+ List<CategoryModel> categories = [
+   CategoryModel(title: 'Technology', image: 'images/technology.jpeg'),
+   CategoryModel(title: 'Health', image: 'images/health.avif'),
+   CategoryModel(title: 'Business', image: 'images/business.avif'),
+   CategoryModel(title: 'Sports', image: 'images/sports.avif'),
+   CategoryModel(title: 'general', image: 'images/general.avif'),
+   CategoryModel(title: 'science', image: 'images/science.avif'),
+   CategoryModel(title: 'Entertainment', image: 'images/entertaiment.avif'),
+ ];
 }
