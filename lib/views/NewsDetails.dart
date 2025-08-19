566527c6924368b0aab6c72d6935d2c9952aaddf
maramrmilad7 <maro12787@gmail.com>
2025-08-19 21:12:15 +0200

    complete the news details screen

--- lib/views/NewsDetails.dart
+++ lib/views/NewsDetails.dart
@@ -2,57 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:newapp/Models/article_model.dart';
 
 class NewsDetails extends StatelessWidget {
-   NewsDetails({super.key});
+  NewsDetails({super.key});
   static String id = 'news_details';
   late ArticleModel articleModel;
 
   @override
   Widget build(BuildContext context) {
-   var articleModel = ModalRoute.of(context)!.settings.arguments as ArticleModel;
+    var articleModel =
+        ModalRoute.of(context)!.settings.arguments as ArticleModel;
     return Scaffold(
-      appBar: AppBar(title: Text('News Details'),
-      centerTitle: true,
-      leading:  IconButton(
+      appBar: AppBar(
+        title: Text('Keep On Reading'),
+        centerTitle: true,
+        leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios),
-        ) ,
-
-        
-       
-      ),
-      body:Column(
-      mainAxisSize: MainAxisSize.min,
-
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Image.network(
-          articleModel.imageUrl!,
-          height: 200,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        )
-    
         ),
-        SizedBox(height: 12),
-        Text(
-          articleModel.title,
-          style: const TextStyle(
-            color: Colors.black87,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert, color: Colors.black),
           ),
-        ),
-        SizedBox(height: 8),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
 
-        Text(
-          articleModel.description??' No description Found',
-          style: const TextStyle(color: Colors.grey, fontSize: 14),
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                articleModel.imageUrl!,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              articleModel.title,
+              style: const TextStyle(
+                color: Colors.black87,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(height: 8),
+
+            Text(
+              articleModel.description ?? ' No description Found',
+              style: const TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.share, color: Colors.black54, size: 40),
+                SizedBox(width: 20),
+                Icon(Icons.bookmark, color: Colors.black54, size: 40),
+              ],
+            ),
+          ],
         ),
-      ],
-    
-      ));
+      ),
+    );
   }
 }
